1c7d542bb061c1e0ce7eb9159d7a42c34757aa07
Clementine Cottineau <clementine.cottineau@iscpif.fr>
2015-05-12 14:30:13 +0100

    implement exposure and isolation indexes

--- src/main/scala/fr/iscpif/schelling/quantity/Simulation.scala
+++ src/main/scala/fr/iscpif/schelling/quantity/Simulation.scala
@@ -13,17 +13,17 @@ object Simulation extends App {
 
   val simulation = new Schelling with RandomState with RandomMoves {
     override def size: Int = 100
-    override def greenRatio: Double = 0.5
-    override def redRatio: Double = 0.5
-    override def maxCapacity: Int = 100
-    override def similarWanted: Double = 0.5
+    override def greenRatio: Double = 0.65
+    override def redRatio: Double = 0.25
+    override def maxCapacity: Int = 10
+    override def similarWanted: Double = 0.45
   }
 
   for {
     (state, step) <- simulation.states.take(100).zipWithIndex
   } {
     def unsatisfied = simulation.unsatisfieds(state).map(_.number).sum
-       println(s"Step $step: Number of unsatisfied: $unsatisfied, dissimilarity index D: ${dissimilarity(state.matrix, Green, Red)}, entropy index H: ${entropy(state.matrix, Green, Red)}")
+       println(s"Step $step: Number of unsatisfied: $unsatisfied, dissimilarity index D: ${dissimilarity(state.matrix, Green, Red)}, entropy index H: ${entropy(state.matrix, Green, Red)}, Exposure of Reds to Greens :${exposureOfColor1ToColor2(state.matrix, Red, Green)},Isolation index of Reds :${isolation(state.matrix, Red, Green)}")
      }
 
 }
--- src/main/scala/fr/iscpif/schelling/quantity/metric/package.scala
+++ src/main/scala/fr/iscpif/schelling/quantity/metric/package.scala
@@ -81,4 +81,51 @@ def entropy(cells: Seq[Seq[Cell]], color1: Color, color2: Color): Double = {
 
     }.sum
   }
+
+
+
+
+def exposureOfColor1ToColor2(cells: Seq[Seq[Cell]], color1: Color, color2: Color): Double = {
+    val flatCells = cells.flatten
+    val totalPopulation = Seq(color1, color2).map{ color => color -> total(color, flatCells) }.toMap
+    val totalPopColor1 = totalPopulation(color1).toDouble
+
+
+    flatCells.map {
+      cell =>
+        val nbColor1 = color1.cellColor.get(cell)
+        val nbColor2 = color2.cellColor.get(cell)
+        val cellPop = nbColor1.toDouble + nbColor2.toDouble
+
+        val cellPropColor2 =
+           if(cellPop.toDouble == 0) 0
+           else nbColor2.toDouble / cellPop.toDouble
+
+        (nbColor1.toDouble / totalPopColor1.toDouble) * cellPropColor2.toDouble
+
+    }.sum
+  }
+
+
+def isolation(cells: Seq[Seq[Cell]], color1: Color, color2: Color): Double = {
+    val flatCells = cells.flatten
+    val totalPopulation = Seq(color1, color2).map{ color => color -> total(color, flatCells) }.toMap
+    val totalPopColor1 = totalPopulation(color1).toDouble
+
+
+    flatCells.map {
+      cell =>
+        val nbColor1 = color1.cellColor.get(cell)
+        val nbColor2 = color2.cellColor.get(cell)
+        val cellPop = nbColor1.toDouble + nbColor2.toDouble
+
+        val cellPropColor1 =
+           if(cellPop.toDouble == 0) 0
+           else nbColor1.toDouble / cellPop.toDouble
+
+        (nbColor1.toDouble / totalPopColor1.toDouble) * cellPropColor1.toDouble
+
+    }.sum
+  }
+
 }
