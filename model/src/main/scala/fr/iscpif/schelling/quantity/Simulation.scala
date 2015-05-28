7352fc79723777eb473a27bd69cf4e67f655b531
Romain Reuillon <romain.reuillon@iscpif.fr>
2015-05-28 13:55:06 +0700

    Merge the 2 loops.

--- model/src/main/scala/fr/iscpif/schelling/quantity/Simulation.scala
+++ model/src/main/scala/fr/iscpif/schelling/quantity/Simulation.scala
@@ -5,6 +5,7 @@ import java.io.File
 import initial._
 import move._
 import metric._
+import stop._
 
 import scalax.io.Resource
 
@@ -14,7 +15,7 @@ object Simulation extends App {
 
   implicit val rng = new Random
 
-  val simulation = new Schelling with RandomState with RandomMoves {
+  val simulation = new Schelling with RandomState with RandomMoves with SpeilmanStop {
     override def size: Int = 50
     override def greenRatio: Double = 0.5
     override def redRatio: Double = 0.35
@@ -22,9 +23,15 @@ object Simulation extends App {
     override def similarWanted: Double = 0.4
   }
 
-  val file1 = new File("/tmp/resultmicro.csv")
+  val dir = "/tmp/"
+
+  val file1 = new File(dir + "resultmicro.csv")
   file1.delete()
   val output1 = Resource.fromFile(file1)
+
+  val file2 = new File(dir + "resultmacro.csv")
+  file2.delete()
+  val output2 = Resource.fromFile(file2)
 
   for {
     (state, step) <- simulation.states.take(100).zipWithIndex
@@ -37,29 +44,16 @@ object Simulation extends App {
       def unsatisfied = Color.all.map { color => simulation.unsatisfied(state, position, color) }.mkString(",")
       output1.append(
         s"""$step,$i,$j,${c.capacity},$agents,$unsatisfied\n""".stripMargin)
+
+      val size = simulation.size
+      val greenRatio = simulation.greenRatio
+      val redRatio = simulation.redRatio
+      val maxCapacity = simulation.maxCapacity
+      val similarWanted = simulation.similarWanted
+
+      output2.append(
+        s"""$step, $unsatisfied,${dissimilarity(state, Green, Red)}, ${moran(state, Red)}, ${entropy(state, Green, Red)}, ${exposureOfColor1ToColor2(state, Red, Green)},${exposureOfColor1ToColor2(state, Green, Red)}, ${isolation(state, Red, Green)}, ${isolation(state, Green, Red)},${delta(state, Red, Green)},${delta(state, Green, Red)}, $size, $greenRatio,$redRatio, $maxCapacity, $similarWanted\n""".stripMargin)
     }
-
-  }
-
-  val file2 = new File("/tmp/resultmacro.csv")
-  file2.delete()
-  val output2 = Resource.fromFile(file2)
-
-  for {
-    (state, step) <- simulation.states.take(100).zipWithIndex
-  } {
-    def unsatisfied = simulation.unsatisfieds(state).map(_.number).sum
-    println(s"Step $step")
-
-    val size = simulation.size
-    val greenRatio = simulation.greenRatio
-    val redRatio = simulation.redRatio
-    val maxCapacity = simulation.maxCapacity
-    val similarWanted = simulation.similarWanted
-
-    output2.append(
-      s"""$step, $unsatisfied,${dissimilarity(state, Green, Red)}, ${moran(state, Red)}, ${entropy(state, Green, Red)}, ${exposureOfColor1ToColor2(state, Red, Green)},${exposureOfColor1ToColor2(state, Green, Red)}, ${isolation(state, Red, Green)}, ${isolation(state, Green, Red)},${delta(state, Red, Green)},${delta(state, Green, Red)}, $size, $greenRatio,$redRatio, $maxCapacity, $similarWanted\n""".stripMargin)
-
   }
 
 }
